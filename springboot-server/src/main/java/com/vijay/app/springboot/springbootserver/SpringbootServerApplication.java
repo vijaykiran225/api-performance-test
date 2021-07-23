9482c255b6fd7998476b1533751bfe189e64898a
Vijay Kiran <vijaykiran225@gmail.com>
2021-07-23 09:49:43 +0530

    init commit

---
+++ springboot-server/src/main/java/com/vijay/app/springboot/springbootserver/SpringbootServerApplication.java
@@ -0,0 +1,13 @@
+package com.vijay.app.springboot.springbootserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServerApplication.class, args);
+	}
+
+}
